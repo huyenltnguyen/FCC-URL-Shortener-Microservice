799aab909af0d559d9d7d3c20d755a43905d9299
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2018-02-15 14:55:40 +0700

    reformat coding style

--- models/result.js
+++ models/result.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 var resultSchema = new mongoose.Schema({
-	original_url: String,
-	short_url: String
+  original_url: String,
+  short_url: String
 });
 
 module.exports = mongoose.model('Result', resultSchema);
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-	res.sendFile('index.html', { root: './views' });
+  res.sendFile('index.html', { root: './views' });
 });
 
 module.exports = router;
